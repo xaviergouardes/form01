8139b67a5952122081c271e299bba3242dc462dd
Xavier Gouardes <xavier.gouardes@laposte.net>
2016-06-17 16:20:43 +0200

    résolution de conflict

--- src/main/java/com/xtechnologies/form01/web/LotController.java
+++ src/main/java/com/xtechnologies/form01/web/LotController.java
@@ -7,17 +7,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
-<<<<<<< HEAD
 public class LotController 
 {
 
 	@RequestMapping(value="/lots", method=RequestMethod.GET)
-=======
-public class TestController 
-{
-
-	@RequestMapping(value="/test", method=RequestMethod.GET)
->>>>>>> refs/remotes/origin/master
     public String greetingForm(Model model) {
         model.addAttribute("test", "test");
         return "index";
